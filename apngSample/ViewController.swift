52ffc6e7a8901ab3bea3df411290df8ea428d6c4
ha1f <ha1f@linecorp.com>
2017-09-21 10:12:26 +0900

    Initial Commit

---
+++ apngSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  apngSample
+//
+//  Created by ST20591 on 2017/09/21.
+//  Copyright © 2017年 ha1f. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
